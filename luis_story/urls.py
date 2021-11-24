43abb740675c700663bcc0a558bfdf552125c3b9
Luis <luis.hector.mg12@gmail.com>
2021-11-24 15:34:35 -0600

    Cover functionality added to Cuentos, starting work on linking cover images to the Story model

--- luis_story/urls.py
+++ luis_story/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('short_stories.urls')),
+    path('photologue/', include('photologue.urls', namespace='photologue')),
 ]
 
 if settings.DEBUG:
