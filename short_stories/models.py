3461c492ed9c07a4d4ac4d922189f8115076f1c7
Luis <luis.hector.mg12@gmail.com>
2021-11-26 09:37:11 -0600

    Genre model implemented, starting work on further styling of the index page

--- short_stories/models.py
+++ short_stories/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+class Genre(models.Model):
+    """Genre to classify the short stories or other kinds of texts."""
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'genres'
+
+    def __str__(self):
+        """Return the name of the genre."""
+        return self.name
+
 class Story(models.Model):
     """The published short story."""
     title = models.CharField(max_length=200)
-    genre = models.CharField(max_length=200)
+    genre =  models.ForeignKey(Genre, on_delete=models.SET_DEFAULT, default="cuento")
     synopsis = models.TextField()
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
@@ -13,5 +25,5 @@ class Story(models.Model):
         verbose_name_plural = 'stories'
 
     def __str__(self):
-        """Return the title of th story."""
+        """Return the title of the story."""
         return self.title
